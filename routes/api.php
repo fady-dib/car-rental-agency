ef7ece96d4f96fc2556ac5d122b89d94b879dd28
fady dib <fady.dib@hotmail.com>
2025-01-04 15:16:20 +0200

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AdminAuthController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Api')->group(function () {
+
+    Route::post('admin/login', [AdminAuthController::class, 'login']);
+    Route::post('login', [AuthController::class, 'login']);
+
 });
