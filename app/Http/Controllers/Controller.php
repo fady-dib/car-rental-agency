9c70f191f2d18e094113a837b6c21b2cfea1d6ea
fady dib <fady.dib@hotmail.com>
2025-01-04 15:12:54 +0200

    Add return error function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function return_error($debugger, $code, $message)
+    {
+        $error['error'] = [];
+        $error['error']['debugger'] = $debugger;
+        $error['error']['code'] = $code;
+        $error['error']['message'] = $message;
+
+        return response()->json($error)->setStatusCode(400);
+    }
 }
