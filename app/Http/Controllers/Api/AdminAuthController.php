35daeba9672e11ad22cecd811a5a74defff70481
fady dib <fady.dib@hotmail.com>
2025-01-04 15:14:12 +0200

    Add admin login function

---
+++ app/Http/Controllers/Api/AdminAuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AdminAuthController extends Controller
+{
+    public function login(Request $request) {
+
+         $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => [
+                'required',
+                'string',
+                'min:6',
+                'regex:/^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).+$/'
+            ],
+        ]);
+
+        if ($validator->fails()) {
+            return parent::return_error(implode(' ', $validator->messages()->all()), 400, 'Invalid credentials');
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth('admin')->attempt($credentials)) {
+            return parent::return_error('Invalid credentials' , 401, 'Invalid credentials');
+        }
+
+        return response()->json([
+            'status' => 'success',
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ]);
+    }
+}
