a575f3cc04f1407f4b63e6639eeed6e90340c582
fady dib <fady.dib@hotmail.com>
2025-01-05 18:44:38 +0200

    Add store file function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -19,4 +19,23 @@ class Controller extends BaseController
 
         return response()->json($error)->setStatusCode(400);
     }
+
+        public function store_file($link, $request_file)
+    {
+        $tmp_file = $request_file;
+        $tmp_file_name = basename($tmp_file->getClientOriginalName(), '.'.$tmp_file->getClientOriginalExtension());
+        $tmp_file_name = strtolower(str_replace(" ", "-", $tmp_file_name));
+        $tmp_file_extension = $tmp_file->getClientOriginalExtension();
+        $original_name = $tmp_file_name . "-" . rand() . "." . $tmp_file_extension;
+
+        if (config('filesystems.default') === 's3') {  
+            Storage::disk('s3')->put('/storage/' . $link . '/' . $original_name, file_get_contents($tmp_file));
+        } else {
+            $tmp_file->storeAs('public/'.$link, $original_name);
+        }
+
+        $file_path = '/storage/' . $link . '/' . $original_name;
+
+        return $file_path;
+    }
 }
